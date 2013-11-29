svn2github/FMILibrary

commit 24356b2046893cec39632eada4010034fd8440f5

FMIL: Remove all of contents of `fmi2_xml_model_structure_t` except `isValidFlag` for #3267, since XML `<ModelStructure>` format has changed.
Also remove contents of function `fmi2_xml_check_model_structure`, which checked the old format.
Function `fmi2_xml_free_model_structure` can currently not get any callbacks and so cannot free the structure; this should be fixed as soon as the structure is filled with new contents.


git-svn-id: https://svn.jmodelica.org/FMILibrary/trunk@5563 7ae51c65-13d5-4921-9d80-6511ddd1419c

src/XML/src/FMI2/fmi2_xml_model_structure.c
@@ -29,26 +29,25 @@ fmi2_xml_model_structure_t* fmi2_xml_allocate_model_structure(jm_callbacks* cb) 
 	fmi2_xml_model_structure_t* ms = (fmi2_xml_model_structure_t*)(cb->calloc(1, sizeof(fmi2_xml_model_structure_t)));
 	if(!ms) return 0;
 
+/*
 	jm_vector_init(jm_voidp)(&ms->inputs,0,cb);
-	jm_vector_init(size_t)(&ms->inputIsDerivative,0,cb); /** \brief If an element is non-zero than this input is a derivative of another input */
+	jm_vector_init(size_t)(&ms->inputIsDerivative,0,cb);
 
 	jm_vector_init(jm_voidp)(&ms->derivatives,0,cb);
-	jm_vector_init(jm_voidp)(&ms->states,0,cb);  /** \brief index of the associated state */
+	jm_vector_init(jm_voidp)(&ms->states,0,cb);
 	
 	jm_vector_init(jm_voidp)(&ms->outputs,0,cb);
-	jm_vector_init(size_t)(&ms->outputIsDerivative,0,cb); /** \brief If an element is non-zero than this output is a derivative of another output */
+	jm_vector_init(size_t)(&ms->outputIsDerivative,0,cb);
+*/
 
 	ms->isValidFlag = 1;
 
-	ms->depsStatesOnInputs = fmi2_xml_allocate_dependencies(cb);
-	ms->depsStatesOnStates = fmi2_xml_allocate_dependencies(cb);
-	ms->depsOutputsOnInputs = fmi2_xml_allocate_dependencies(cb);
-	ms->depsOutputsOnStates = fmi2_xml_allocate_dependencies(cb);
-
+/*
 	if(!ms->depsStatesOnInputs || !ms->depsStatesOnStates || !ms->depsOutputsOnInputs || !ms->depsOutputsOnStates) {
 		fmi2_xml_free_model_structure(ms);
 		return 0;
 	}
+*/
 
 	return ms;
 }
@@ -56,21 +55,20 @@ fmi2_xml_model_structure_t* fmi2_xml_allocate_model_structure(jm_callbacks* cb) 
 void fmi2_xml_free_model_structure(fmi2_xml_model_structure_t* ms) {
 	jm_callbacks* cb;
 	if(!ms) return;
-	cb = ms->inputs.callbacks;
+/*	cb = ms->inputs.callbacks;*/
+
+/*
 	jm_vector_free_data(jm_voidp)(&ms->inputs);
-	jm_vector_free_data(size_t)(&ms->inputIsDerivative); /** \brief If an element is non-zero than this input is a derivative of another input */
+	jm_vector_free_data(size_t)(&ms->inputIsDerivative);
 
 	jm_vector_free_data(jm_voidp)(&ms->derivatives);
-	jm_vector_free_data(jm_voidp)(&ms->states);  /** \brief index of the associated state */
+	jm_vector_free_data(jm_voidp)(&ms->states);
 	
 	jm_vector_free_data(jm_voidp)(&ms->outputs);
-	jm_vector_free_data(size_t)(&ms->outputIsDerivative); /** \brief If an element is non-zero than this output is a derivative of another output */
+	jm_vector_free_data(size_t)(&ms->outputIsDerivative);
+*/
 	
-	fmi2_xml_free_dependencies(ms->depsStatesOnInputs); /** \brief Dependencies between vars. */
-	fmi2_xml_free_dependencies(ms->depsStatesOnStates);
-	fmi2_xml_free_dependencies(ms->depsOutputsOnInputs); /** \brief Dependencies between vars.  */
-	fmi2_xml_free_dependencies(ms->depsOutputsOnStates);
-	cb->free(ms);
+/*	cb->free(ms); TODO: free this again! */
 }
 
 /*
@@ -133,178 +131,10 @@ void fmi2_xml_free_dependencies(fmi2_xml_dependencies_t* dep) {
 
 
 int fmi2_xml_check_model_structure(fmi2_xml_model_description_t* md) {
-    size_t i, numvar, numStates, numInputs, numOutputs;
 	fmi2_xml_model_structure_t* ms = md->modelStructure;
 	jm_callbacks* cb = md->callbacks;
 
 	if(!ms || !ms->isValidFlag) return 0;
-
-    numvar = jm_vector_get_size(jm_named_ptr)(&md->variablesByName);
-	numStates = jm_vector_get_size(jm_voidp)(&ms->states);
-	numInputs = jm_vector_get_size(jm_voidp)(&ms->inputs);
-	numOutputs = jm_vector_get_size(jm_voidp)(&ms->outputs);
-
-	/* check that all inputs and outputs are listed and that states
-		are not aliased to inputs */
-    for(i = 0; i< numvar; i++) {
-		jm_named_ptr named = jm_vector_get_item(jm_named_ptr)(&md->variablesByName, i);
-        fmi2_xml_variable_t* v = named.ptr;
-        if(v->causality == fmi2_causality_enu_input) {
-			if(v->inputIndex == 0) {
-				jm_log_error(cb, module, "Input variable %s is not listed among inputs in ModelStructure", fmi2_xml_get_variable_name(v));
-				ms->isValidFlag = 0;
-			}
-		}
-		else if(v->causality == fmi2_causality_enu_output) {
-			if(v->outputIndex == 0) {
-				jm_log_error(cb, module, "Output variable %s is not listed among outputs in ModelStructure", fmi2_xml_get_variable_name(v));
-				ms->isValidFlag = 0;
-			}
-		}
-		if(v->stateIndex != 0) {
-			fmi2_xml_variable_t* vb = fmi2_xml_get_variable_alias_base(md, v);
-			if((vb->causality == fmi2_causality_enu_input) || (vb->causality == fmi2_causality_enu_parameter)) {
-				if(v == vb) {
-					jm_log_error(cb, module, "Variable %s with causality %s is referred as a state in ModelStructure", 
-						fmi2_xml_get_variable_name(v), fmi2_causality_to_string((fmi2_causality_enu_t)v->causality));
-					ms->isValidFlag = 0;
-				}
-				else {
-					jm_log_error(cb, module, "Variable %s aliased to %s with causality %s is referred as a state in ModelStructure", 
-						fmi2_xml_get_variable_name(v), fmi2_xml_get_variable_name(vb), fmi2_causality_to_string((fmi2_causality_enu_t)v->causality));
-					ms->isValidFlag = 0;
-				}
-			}
-			if(vb->variability <= fmi2_variability_enu_discrete) {
-				if(v == vb) {
-					jm_log_error(cb, module, "Variable %s with variability %s is referred as a state in ModelStructure", 
-						fmi2_xml_get_variable_name(v), fmi2_variability_to_string((fmi2_variability_enu_t)v->variability));
-					ms->isValidFlag = 0;
-				}
-				else {
-					jm_log_error(cb, module, "Variable %s aliased to %s with variability %s is referred as a state in ModelStructure", 
-						fmi2_xml_get_variable_name(v), fmi2_xml_get_variable_name(vb), 
-						fmi2_variability_to_string((fmi2_variability_enu_t)v->variability));
-					ms->isValidFlag = 0;
-				}
-			}
-		}
-    }
-	/*  check that "derivate" attribute is not out of range */
-    for(i = 0; i< numInputs; i++) {
-		size_t ind = jm_vector_get_item(size_t)(&ms->inputIsDerivative, i);
-		if(ind > numInputs) {
-			fmi2_xml_variable_t* v= (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->inputs, i);
-			jm_log_error(cb, module, "Input %s indicates derivative index %u but number of inputs is %u", 
-						fmi2_xml_get_variable_name(v), ind, numInputs);
-			ms->isValidFlag = 0;
-		}
-	}
-    for(i = 0; i< numOutputs; i++) {
-		size_t ind = jm_vector_get_item(size_t)(&ms->outputIsDerivative, i);
-		if(ind > numOutputs) {
-			fmi2_xml_variable_t* v= (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->outputs, i);
-			jm_log_error(cb, module, "Output %s indicates derivative index %u but number of outputs is %u", 
-						fmi2_xml_get_variable_name(v), ind, numInputs);
-			ms->isValidFlag = 0;
-		}
-	}
-
-	/* check dependencies on inputs */
-	fmi2_xml_zero_empty_dependencies(&ms->depsStatesOnInputs);
-	if(ms->depsStatesOnInputs) {
-		fmi2_xml_dependencies_t* dsi = ms->depsStatesOnInputs;
-		size_t varIndex = 0;
-		size_t numDeps = jm_vector_get_size(size_t)(&dsi->dependencyIndex);
-		jm_vector_set_item(size_t)(&dsi->startIndex,numStates, numDeps);
-		for(i = 0; i < numDeps; i++) {
-			size_t ind = jm_vector_get_item(size_t)(&dsi->dependencyIndex, i);
-			if(ind > numInputs) {
-				fmi2_xml_variable_t* v;
-				while(jm_vector_get_item(size_t)(&dsi->startIndex, varIndex+1) < i) varIndex++;
-				v = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->derivatives, varIndex);
-				jm_log_error(cb, module, "Dependency list for state derivative %s contains input index %u but the number of inputs is %u",
-					fmi2_xml_get_variable_name(v), ind, numInputs);
-				ms->isValidFlag = 0;
-			}
-		}
-	}
-	fmi2_xml_zero_empty_dependencies(&ms->depsOutputsOnInputs);
-	if(ms->depsOutputsOnInputs) {
-		fmi2_xml_dependencies_t* doi = ms->depsOutputsOnInputs;
-		size_t varIndex = 0;
-		size_t numDeps = jm_vector_get_size(size_t)(&doi->dependencyIndex);
-		jm_vector_set_item(size_t)(&doi->startIndex,numOutputs, numDeps);
-		for(i = 0; i < numDeps; i++) {
-			size_t ind = jm_vector_get_item(size_t)(&doi->dependencyIndex, i);
-			fmi2_xml_variable_t* vOut;
-			while(jm_vector_get_item(size_t)(&doi->startIndex, varIndex+1) < i) varIndex++;
-			vOut = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->outputs, varIndex);
-			if(ind > numInputs) {
-				jm_log_error(cb, module, "Dependency list for output %s contains input index %u but the number of inputs is %u",
-					fmi2_xml_get_variable_name(vOut), ind, numInputs);
-				ms->isValidFlag = 0;
-			}
-			else if(ind!=0) {
-				fmi2_xml_variable_t* vIn = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->inputs, ind - 1);
-				fmi2_variability_enu_t variOut = fmi2_xml_get_variability(vOut);
-				fmi2_variability_enu_t variIn = fmi2_xml_get_variability(vIn);
-				if((variOut <= fmi2_variability_enu_tunable) && (variOut > variIn)) {
-					jm_log_error(cb, module, "Output %s with variability %s depends on input %s with variability %s",
-						fmi2_xml_get_variable_name(vOut), fmi2_variability_to_string(variOut), 
-						fmi2_xml_get_variable_name(vIn), fmi2_variability_to_string(variIn));
-					ms->isValidFlag = 0;
-				}
-			}
-		}
-	}
-	/* check dependencies on states */
-	fmi2_xml_zero_empty_dependencies(&ms->depsStatesOnStates);
-	if(ms->depsStatesOnStates) {
-		fmi2_xml_dependencies_t* dss = ms->depsStatesOnStates;
-		size_t varIndex = 0;
-		size_t numDeps = jm_vector_get_size(size_t)(&dss->dependencyIndex);
-		jm_vector_set_item(size_t)(&dss->startIndex,numStates, numDeps);
-		for(i = 0; i < numDeps; i++) {
-			size_t ind = jm_vector_get_item(size_t)(&dss->dependencyIndex, i);
-			if(ind > numStates) {
-				fmi2_xml_variable_t* v;
-				while(jm_vector_get_item(size_t)(&dss->startIndex, varIndex+1) < i) varIndex++;
-				v = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->derivatives, varIndex);
-				jm_log_error(cb, module, "Dependency list for state derivative %s contains state index %u but the number of states is %u",
-					fmi2_xml_get_variable_name(v), ind, numStates);
-				ms->isValidFlag = 0;
-			}
-		}
-	}
-	fmi2_xml_zero_empty_dependencies(&ms->depsOutputsOnStates);
-	if(ms->depsOutputsOnStates) {
-		fmi2_xml_dependencies_t* dos = ms->depsOutputsOnStates;
-		size_t varIndex = 0;
-		size_t numDeps = jm_vector_get_size(size_t)(&dos->dependencyIndex);
-		jm_vector_set_item(size_t)(&dos->startIndex,numOutputs, numDeps);
-		for(i = 0; i < numDeps; i++) {
-			size_t ind = jm_vector_get_item(size_t)(&dos->dependencyIndex, i);
-			fmi2_xml_variable_t* vOut;
-			while(jm_vector_get_item(size_t)(&dos->startIndex, varIndex+1) < i) varIndex++;
-			vOut = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->outputs, varIndex);
-			if(ind > numStates ) {
-				jm_log_error(cb, module, "Dependency list for output %s contains state index %u but the number of states is %u",
-					fmi2_xml_get_variable_name(vOut), ind, numStates);
-				ms->isValidFlag = 0;
-			}
-			else if(ind!=0) {
-				fmi2_xml_variable_t* vState = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(&ms->states, ind - 1 );
-				fmi2_variability_enu_t variOut = fmi2_xml_get_variability(vOut);
-				if(variOut <= fmi2_variability_enu_tunable) {
-					jm_log_error(cb, module, "Output %s with variability %s depends on continuous state %s",
-						fmi2_xml_get_variable_name(vOut), fmi2_variability_to_string(variOut), 
-						fmi2_xml_get_variable_name(vState));
-					ms->isValidFlag = 0;
-				}
-			}
-		}
-	}
 
 	return ms->isValidFlag;
 }

src/XML/src/FMI2/fmi2_xml_model_structure_impl.h
@@ -45,19 +45,16 @@ fmi2_xml_dependencies_t* fmi2_xml_allocate_dependencies(jm_callbacks* cb);
 void fmi2_xml_free_dependencies(fmi2_xml_dependencies_t* dep);
 	
 struct fmi2_xml_model_structure_t {
+/*
 	jm_vector(jm_voidp) inputs;
-	jm_vector(size_t) inputIsDerivative; /** \brief If an element is non-zero than this input is a derivative of another input */
+	jm_vector(size_t) inputIsDerivative;
 
 	jm_vector(jm_voidp) derivatives;
-	jm_vector(jm_voidp) states;  /** \brief index of the associated state */
-
-	fmi2_xml_dependencies_t* depsStatesOnInputs; /** \brief Dependencies between vars. */
-	fmi2_xml_dependencies_t* depsStatesOnStates;
+	jm_vector(jm_voidp) states;
 
 	jm_vector(jm_voidp) outputs;
-	jm_vector(size_t) outputIsDerivative; /** \brief If an element is non-zero than this output is a derivative of another output */
-	fmi2_xml_dependencies_t*  depsOutputsOnInputs; /** \brief Dependencies between vars.  */
-	fmi2_xml_dependencies_t*  depsOutputsOnStates;
+	jm_vector(size_t) outputIsDerivative;
+*/
 
 	int isValidFlag;  /**\ brief The flag is used to signal if an error was discovered and the model structure is not usable */
 };

commit 210eb87a4522e9f49dd25a88a3b8f586c8b6eba4

FMIL: Begin parsing `<ModeStructure><Outputs>` for #3267, and add in vectors to store referenced variables.
Also fix memory leak temporarily introduced in r5563.

git-svn-id: https://svn.jmodelica.org/FMILibrary/trunk@5586 7ae51c65-13d5-4921-9d80-6511ddd1419c

src/XML/src/FMI2/fmi2_xml_model_structure.c
@@ -29,16 +29,10 @@ fmi2_xml_model_structure_t* fmi2_xml_allocate_model_structure(jm_callbacks* cb) 
 	fmi2_xml_model_structure_t* ms = (fmi2_xml_model_structure_t*)(cb->calloc(1, sizeof(fmi2_xml_model_structure_t)));
 	if(!ms) return 0;
 
-/*
-	jm_vector_init(jm_voidp)(&ms->inputs,0,cb);
-	jm_vector_init(size_t)(&ms->inputIsDerivative,0,cb);
-
-	jm_vector_init(jm_voidp)(&ms->derivatives,0,cb);
-	jm_vector_init(jm_voidp)(&ms->states,0,cb);
-	
 	jm_vector_init(jm_voidp)(&ms->outputs,0,cb);
-	jm_vector_init(size_t)(&ms->outputIsDerivative,0,cb);
-*/
+	jm_vector_init(jm_voidp)(&ms->derivatives,0,cb);
+	jm_vector_init(jm_voidp)(&ms->discreteStates,0,cb);
+	jm_vector_init(jm_voidp)(&ms->initialUnknowns,0,cb);
 
 	ms->isValidFlag = 1;
 
@@ -55,20 +49,14 @@ fmi2_xml_model_structure_t* fmi2_xml_allocate_model_structure(jm_callbacks* cb) 
 void fmi2_xml_free_model_structure(fmi2_xml_model_structure_t* ms) {
 	jm_callbacks* cb;
 	if(!ms) return;
-/*	cb = ms->inputs.callbacks;*/
+	cb = ms->outputs.callbacks;
 
-/*
-	jm_vector_free_data(jm_voidp)(&ms->inputs);
-	jm_vector_free_data(size_t)(&ms->inputIsDerivative);
-
-	jm_vector_free_data(jm_voidp)(&ms->derivatives);
-	jm_vector_free_data(jm_voidp)(&ms->states);
-	
 	jm_vector_free_data(jm_voidp)(&ms->outputs);
-	jm_vector_free_data(size_t)(&ms->outputIsDerivative);
-*/
+	jm_vector_free_data(jm_voidp)(&ms->derivatives);
+	jm_vector_free_data(jm_voidp)(&ms->discreteStates);
+	jm_vector_free_data(jm_voidp)(&ms->initialUnknowns);
 	
-/*	cb->free(ms); TODO: free this again! */
+	cb->free(ms);
 }
 
 /*
@@ -152,13 +140,13 @@ int fmi2_xml_handle_ModelStructure(fmi2_xml_parser_context_t *context, const cha
     }
     else {
 		/** make sure model structure information is consistent */
-/*
+
 		if(!fmi2_xml_check_model_structure(md)) {
 			fmi2_xml_parse_fatal(context, "Model structure is not valid due to detected errors. Cannot continue.");
 			return -1;
 		}
-		md->numberOfContinuousStates = jm_vector_get_size(jm_voidp)(&md->modelStructure->states);
-*/
+/*		md->numberOfContinuousStates = jm_vector_get_size(jm_voidp)(&md->modelStructure->states); */
+
     }
     return 0;
 }
@@ -201,6 +189,30 @@ int fmi2_xml_handle_InitialUnknowns(fmi2_xml_parser_context_t *context, const ch
 /*int fmi2_xml_handle_OutputUnknown(fmi2_xml_parser_context_t *context, const char* data) {*/
 int fmi2_xml_handle_Unknown(fmi2_xml_parser_context_t *context, const char* data) {
     if(!data) {
+        fmi2_xml_model_description_t* md = context->modelDescription;
+        fmi2_xml_model_structure_t* ms = md->modelStructure;
+
+        unsigned int index;
+        fmi2_xml_variable_t* variable;
+
+        /* <xs:attribute name="index" type="xs:unsignedInt" use="required"> */        
+        if (fmi2_xml_set_attr_uint(context, fmi2_xml_elmID_Unknown, fmi_attr_id_index, 1, &index, 0)) return -1;
+        index--; /* Convert from one- to zero-based indexing */
+
+        /* Ok to just check upper bound since index is unsigned. */
+        if (index >= jm_vector_get_size(jm_voidp)(md->variablesOrigOrder)) {
+            fmi2_xml_parse_error(context, "The index attribute must have a value between 1 and the number of model variables.");
+            ms->isValidFlag = 0;
+            return -1;
+        }
+        variable = (fmi2_xml_variable_t*)jm_vector_get_item(jm_voidp)(md->variablesOrigOrder, index);
+        
+        if (!jm_vector_push_back(jm_voidp)(&ms->outputs, variable)) {
+            fmi2_xml_parse_fatal(context, "Could not allocate memory");
+            ms->isValidFlag = 0;
+            return -1;
+        }
+        
     }
     else {
     }

src/XML/src/FMI2/fmi2_xml_model_structure_impl.h
@@ -45,16 +45,10 @@ fmi2_xml_dependencies_t* fmi2_xml_allocate_dependencies(jm_callbacks* cb);
 void fmi2_xml_free_dependencies(fmi2_xml_dependencies_t* dep);
 	
 struct fmi2_xml_model_structure_t {
-/*
-	jm_vector(jm_voidp) inputs;
-	jm_vector(size_t) inputIsDerivative;
-
-	jm_vector(jm_voidp) derivatives;
-	jm_vector(jm_voidp) states;
-
 	jm_vector(jm_voidp) outputs;
-	jm_vector(size_t) outputIsDerivative;
-*/
+	jm_vector(jm_voidp) derivatives;
+	jm_vector(jm_voidp) discreteStates;
+	jm_vector(jm_voidp) initialUnknowns;
 
 	int isValidFlag;  /**\ brief The flag is used to signal if an error was discovered and the model structure is not usable */
 };